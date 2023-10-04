c26134996c2e872527a45fa71e10cdd37b6ef0b7
ck <chunkind.com@naver.com>
2023-10-04 17:02:23 +0900

    메인 디자인

--- september_front/src/com/ck/september/layout/Main.js
+++ september_front/src/com/ck/september/layout/Main.js
@@ -4,20 +4,20 @@ import { Button, AppBar, Backdrop, Flex, Box, HStack, VStack, Wrap, TextInput } 
 import Icon from 'react-native-vector-icons/FontAwesome';
 
 export default class Main extends Component{
-
-  
-
   render(){
     return(
       <VStack>
         <AppBar title = "Main"/>
-        <HStack>
-          <TextInput label='label' leading={props => <Icon name="pausecircleo" size={24} color="#000000" />} />
+        <HStack style={{ justifyContent:"center", alignItems:"center"}}>
+          <Box p={4} style={{backgroundColor:"white", width:"100%"}}>
+            <TextInput label='label' variant="outlined" leading={props => <Icon name="pausecircleo" size={24} />} style={{padding:"14px"}} />
+            <TextInput label='label' variant="outlined" leading={props => <Icon name="pausecircleo" size={24} />} style={{padding:"14px"}} />
+          </Box>
         </HStack>
-        <Wrap p={5} spacing={6}>
+        <HStack p={5} pb={15} spacing={6} style={{backgroundColor:"white", width:"100%", justifyContent:"center", alignItems:"flex-end"}}>
             <Button title="로그인" />
             <Button title="회원가입" />
-        </Wrap>
+          </HStack>
       </VStack>
     )
   }
